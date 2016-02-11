32000e68e1da3e59a9938d2e320c2a7975ae82dd
Nikolaj Mariager <nikolaj.mariager@gmail.com>
2016-02-11 13:38:15 +0100

    Added unit tests for Framework. Fixed usings. Fixed references.

---
+++ ThreeOneSevenBee.Development/ThreeOneSevenBee.Framework.Tests/App.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Bridge;
+using Bridge.Html5;
+using Bridge.QUnit;
+using ThreeOneSevenBee.Framework.Euclidean;
+
+namespace ThreeOneSevenBee.Framework.Tests
+{
+    public class App
+    {
+        [Ready]
+        public static void Main()
+        {
+            QUnit.Module("Euclidean");
+            QUnit.Test("Constructor Vector2", (assert) =>
+            {
+                assert.Expect(1);
+
+                var vectorZero = new Vector2(0, 0);
+                assert.Ok(Math.Abs(vectorZero.X) < double.Epsilon && Math.Abs(vectorZero.Y) < double.Epsilon, "Vector2 Zero");
+            });
+        }
+    }
+}
