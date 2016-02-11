0edf96faf5932a741865a57ff754f9b124b3bb43
Nikolaj Mariager <nikolaj.mariager@gmail.com>
2016-02-11 10:58:03 +0100

    Initial code.

---
+++ ThreeOneSevenBee.Development/ThreeOneSevenBee.Prototype/App.cs
@@ -0,0 +1,17 @@
+﻿using Bridge;
+using Bridge.Html5;
+using ThreeOneSevenBee.Framework;
+
+namespace ThreeOneSevenBee.Prototype
+{
+    public class App
+    {
+        [Ready]
+        public static void Main()
+        {
+            var canvas = Document.GetElementById<CanvasElement>("canvas");
+            var game = new PrototypeGame(canvas);
+            game.Run();
+        }
+    }
+}
---
+++ ThreeOneSevenBee.Development/ThreeOneSevenBee.Prototype/PrototypeGame.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bridge.Html5;
+using ThreeOneSevenBee.Framework;
+
+namespace ThreeOneSevenBee.Prototype
+{
+    class PrototypeGame : Game
+    {
+        public PrototypeGame(CanvasElement canvas)
+            : base(canvas)
+        {
+
+        }
+
+        public override void Draw(double deltaTime, double totalTime)
+        {
+            Context2D.ClearRect(0, 0, 800, 600);
+
+            Context2D.FillStyle = HTMLColor.CornflowerBlue;
+            Context2D.FillRect(0, 0, 800, 600);
+
+            Context2D.FillStyle = HTMLColor.Black;
+            Context2D.Font = "20px Arial";
+            Context2D.FillText("Hello, World!", 50, 50);
+
+            base.Draw(deltaTime, totalTime);
+        }
+    }
+}
