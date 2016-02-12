1adb0ba6c19d1bd0447c64dbb1c76f69ce8a7de0
AntonChristensen server <anton.christensen9700@gmail.com>
2016-02-12 08:56:16 -0500

    fixes minor bug in server compile script

--- srv/compile.sh
+++ srv/compile.sh
@@ -1,3 +1,4 @@
+cd "$(dirname "$0")"
 find ../ThreeOneSevenBee.Development/ThreeOneSevenBee.Framework/ -type f | grep -i cs$ | xargs -i cp {} .
 find ../ThreeOneSevenBee.Development/ThreeOneSevenBee.Prototype/ -type f | grep -i cs$ | xargs -i cp {} .
 build/builder.sh
