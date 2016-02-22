f9ffb51d510ae2b75b0eea54398888392dfe8bbe
Nikolaj Mariager <nikolaj.mariager@gmail.com>
2016-02-22 19:35:52 +0100

    Added preliminary Pointer class.

---
+++ ThreeOneSevenBee.Development/ThreeOneSevenBee.Framework/Html/Pointer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeOneSevenBee.Framework.Html
+{
+    /// <summary>
+    /// A unified Touch/Mouse input system.
+    /// </summary>
+    /// <remarks>Loosely based on https://developer.mozilla.org/en-US/docs/Web/API/PointerEvent. </remarks>
+    public class Pointer
+    {
+        public Pointer(int id, PointerTypes type)
+        {
+            this.Id = id;
+            this.Type = type;
+        }
+
+        public int Id { get; private set; }
+
+        public PointerTypes Type { get; private set; }
+    }
+
+    public enum PointerTypes
+    {
+        Mouse,
+        Touch
+    }
+}
