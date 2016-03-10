b803cf192ab94ef83287a7ae1f3b735f6d4dbee6
Morten R <morten.rask@live.dk>
2016-03-10 10:21:33 +0100

    Updates files in website

---
+++ ThreeOneSevenBee.Frontend.Website/js/threeOneSevenBee.model.expression.expressionRules.js
@@ -0,0 +1,35 @@
+﻿(function (globals) {
+    "use strict";
+
+    Bridge.define('ThreeOneSevenBee.Model.Expression.ExpressionRules.Rules', {
+        statics: {
+            itselfRule: function (expression, selection, identity) {
+                identity.v = expression;
+                return true;
+            },
+            commutativeRule: function (expression, selection, identity) {
+                var $t;
+                var operatorExpression;
+                var serializer = new ThreeOneSevenBee.Model.Expression.ExpressionSerializer();
+                if (Bridge.hasValue((($t = Bridge.as(expression, ThreeOneSevenBee.Model.Expression.Expressions.OperatorExpression), operatorExpression = $t, $t)))) {
+                    if (operatorExpression.getType() === ThreeOneSevenBee.Model.Expression.Expressions.OperatorType.add) {
+                        identity.v = serializer.deserialize(serializer.serialize(operatorExpression.getRight()) + "+" + serializer.serialize(operatorExpression.getLeft()));
+                        return true;
+                    }
+                    else  {
+                        if (operatorExpression.getType() === ThreeOneSevenBee.Model.Expression.Expressions.OperatorType.multiply) {
+                            identity.v = serializer.deserialize(serializer.serialize(operatorExpression.getRight()) + "*" + serializer.serialize(operatorExpression.getLeft()));
+                            return true;
+                        }
+                    }
+                }
+                identity.v = null;
+                return false;
+            }
+        }
+    });
+    
+    
+    
+    Bridge.init();
+})(this);
---
+++ ThreeOneSevenBee.Frontend.Website/js/threeOneSevenBee.model.observable.js
@@ -0,0 +1,35 @@
+﻿(function (globals) {
+    "use strict";
+
+    Bridge.define('ThreeOneSevenBee.Model.Observable.ObservableBase$1', function (T) { return {
+        inherits: [ThreeOneSevenBee.Model.ObserverPattern.IObservable$1(T)],
+        data: null,
+        observers: null,
+        constructor: function (data) {
+            this.observers = new Bridge.List$1(ThreeOneSevenBee.Model.ObserverPattern.IObserver$1(T))();
+            this.data = data;
+        },
+        getData: function () {
+            return this.data;
+        },
+        setData: function (value) {
+            this.data = value;
+            this.updateObservers();
+        },
+        addObserver: function (observer) {
+            this.observers.add(observer);
+        },
+        updateObservers: function () {
+            var $t;
+            $t = Bridge.getEnumerator(this.observers);
+            while ($t.moveNext()) {
+                var observer = $t.getCurrent();
+                observer.update(this.data);
+            }
+        }
+    }; });
+    
+    
+    
+    Bridge.init();
+})(this);
