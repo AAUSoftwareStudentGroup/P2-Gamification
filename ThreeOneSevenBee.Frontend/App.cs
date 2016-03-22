2acb9d31c1b907057e8ed45abcd6b6bfdeb99c10
Morten R <morten.rask@live.dk>
2016-03-22 08:31:53 +0100

    Removes console writes

--- ThreeOneSevenBee.Frontend/App.cs
+++ ThreeOneSevenBee.Frontend/App.cs
@@ -12,26 +12,23 @@ namespace ThreeOneSevenBee.Frontend
         [Ready]
         public static void Main()
         {
-            var t = new Template();
-            Console.WriteLine(t.ToString());
-
             CanvasElement canvas = Document.GetElementById<CanvasElement>("canvas");
 
             CanvasContext context = new CanvasContext(canvas);
 
-            ExpressionModel model = new ExpressionModel("{b^3*a^2}/{a^5*b^2}", Rules.DivideRule, Rules.ExponentToProductRule,Rules.ProductToExponentRule);
+            ExpressionModel model = new ExpressionModel("{b^3*a^2}/{a^5*b^2}", 
+                Rules.DivideRule, Rules.ExponentToProductRule,Rules.ProductToExponentRule);
 
             model.Expression.Clone();
 
             View view = new CompositeView(600, 400)
             {
-                new ExpressionView(model, 600, 300) { X = 0, Y = 0 },
+                new ExpressionView(model, 100, 300) { X = 250, Y = 0 },
                 new IdentityMenuView(model, 600, 100) { Y = 300 }
             };
             
             context.SetContentView(view);
             model.OnChanged += (m) => context.Draw();
-            //model.OnChanged += (m) => m.Expression.PrettyPrint();
             context.Draw();
         }
     }
