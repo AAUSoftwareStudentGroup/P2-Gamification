5a07d0f79a1cdb4db9fe9584ee2b8b0931b6f29b
Morten R <morten.rask@live.dk>
2016-03-30 15:34:27 +0200

    Adds GameModel

---
+++ ThreeOneSevenBee.Model/Game/LevelCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ThreeOneSevenBee.Model.Game
+{
+    public class LevelCategory
+    {
+        public string Name;
+        List<Level> Levels;
+    }
+}
