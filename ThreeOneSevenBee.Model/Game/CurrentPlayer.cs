cfa888cf35dfb0b343bf58ad777410bd650cd8bc
Flarenityy <mathias@ibsen.name>
2016-03-30 12:49:06 +0200

    Adds Player and CurrentPlayer to Game

---
+++ ThreeOneSevenBee.Model/Game/CurrentPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ThreeOneSevenBee.Model.Game
+{
+    class CurrentPlayer : Player
+    {
+        public CurrentPlayer(string player) : base(player){ }
+
+        public List<Level> CompletedLevels = new List<Level>();
+
+
+    }
+}
---
+++ ThreeOneSevenBee.Model/Game/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ThreeOneSevenBee.Model.Game
+{
+    class Player {
+
+        public Player(string playername)
+        {
+            this.PlayerName = playername;
+        }
+
+        public List<string> Badges = new List<string>();
+
+        public string PlayerName { get; private set; }
+
+        public string LastLoginTime { get; set; }
+
+      
+    }
+}
