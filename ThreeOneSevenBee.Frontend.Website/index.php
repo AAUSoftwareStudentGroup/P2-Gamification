ad4be034d06210fe3bc739f6d060b00bc34fe81c
Anton Christensen <anton.christensen9700@gmail.com>
2016-03-30 13:08:02 +0200

    adds index.php

---
+++ ThreeOneSevenBee.Frontend.Website/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>317B</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <script src="js/bridge.js"></script>
+
+    <script src="js/threeOneSevenBee.model.uI.js"></script>
+    <script src="js/threeOneSevenBee.model.expression.js"></script>
+    <script src="js/threeOneSevenBee.model.expression.expressionRules.js"></script>
+    <script src="js/threeOneSevenBee.model.collections.js"></script>
+    <script src="js/threeOneSevenBee.model.expression.expressions.js"></script>
+    <script src="js/threeOneSevenBee.model.js"></script>
+    <script src="js/threeOneSevenBee.frontend.js"></script>
+    <script src="js/threeOneSevenBee.model.euclidean.js"></script>
+    <script src="js/threeOneSevenBee.model.geometry.js"></script>
+    <script src="js/threeOneSevenBee.model.polygon.js"></script>
+</head>
+<body style="margin: 0;overflow:hidden">
+    <canvas id="canvas" width="600" height="400"></canvas>
+</body>
+</html>
