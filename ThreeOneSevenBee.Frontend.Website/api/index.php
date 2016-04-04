4e19267bfc7d2a7f3bea689bb096cc0b99d204dd
Anton Christensen <anton.christensen9700@gmail.com>
2016-04-04 11:29:22 +0200

    Increases security on api calls

--- ThreeOneSevenBee.Frontend.Website/api/index.php
+++ ThreeOneSevenBee.Frontend.Website/api/index.php
@@ -8,49 +8,50 @@ if(isset($_GET['action']))
 elseif(isset($_POST['action']))
     $IN = $_POST;
 
-
 if($IN != null)
-    $IN['action']($IN, $db);
+    API::$IN['action']($IN, $db);
 else
     respond(false, null, "no action given");
 
-function respond($success = true, $data = null, $message = null) {
-    echo ("{".
-            "success: ".($success ? "true" : "false").
-            ($data    != null ? ",data: ".json_encode($data) : "").
-            ($message != null ? ",message: \"".$message."\"" : "").
-          "}");
-}
+class API {
+    static function respond($success = true, $data = null, $message = null) {
+        echo ("{".
+                "success: ".($success ? "true" : "false").
+                ($data    != null ? ",data: ".json_encode($data) : "").
+                ($message != null ? ",message: \"".$message."\"" : "").
+              "}");
+    }
 
-function user_logout($IN, $db) {
-    session_start();
-    if(isset($_SESSION['authorized']))
-        unset($_SESSION['authorized']);
-    respond();
-}
+    static function user_logout($IN, $db) {
+        session_start();
+        if(isset($_SESSION['authorized']))
+            unset($_SESSION['authorized']);
+        API::respond();
+    }
 
-// function delete_user_by_id($IN, $db) {
-//     $db->query("UPDATE gamedb.user
-//                   SET deleted_at=UNIX_TIMESTAMP()
-//                   WHERE id=?;", $IN['user_id']
-//                 );
-//     respond();
-// }
+    // static function delete_user_by_id($IN, $db) {
+    //     $db->query("UPDATE gamedb.user
+    //                   SET deleted_at=UNIX_TIMESTAMP()
+    //                   WHERE id=?;", $IN['user_id']
+    //                 );
+    //     respond();
+    // }
 
-function delete_class_by_id($IN, $db) {
-    $db->query("UPDATE gamedb.class
-                  SET deleted_at=UNIX_TIMESTAMP()
-                  WHERE id=?;", $IN['class_id']
-                );
-    respond();
-}
+    static function delete_class_by_id($IN, $db) {
+        $db->query("UPDATE gamedb.class
+                      SET deleted_at=UNIX_TIMESTAMP()
+                      WHERE id=?;", $IN['class_id']
+                    );
+        API::respond();
+    }
 
-function delete_school_by_id($IN, $db) {
-    $db->query("UPDATE gamedb.school
-                  SET deleted_at=UNIX_TIMESTAMP()
-                  WHERE id=?;", $IN['school_id']
-                );
-    respond();
+    static function delete_school_by_id($IN, $db) {
+        $db->query("UPDATE gamedb.school
+                      SET deleted_at=UNIX_TIMESTAMP()
+                      WHERE id=?;", $IN['school_id']
+                    );
+        API::respond();
+    }
 }
 
 ?>
