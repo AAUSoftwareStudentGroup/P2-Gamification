e4bb16ad1a05701653d5a73db93119f87dafbbe6
Nikolaj Mariager <nikolaj.mariager@gmail.com>
2016-04-05 12:09:47 +0200

    #23 - Added test for ProductToExponentRule and ExponentToProductRule.

---
+++ ThreeOneSevenBee.ModelTests/RulesTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThreeOneSevenBee.Model.Expression;
+using ThreeOneSevenBee.Model.Expression.ExpressionRules;
+using System.Collections.Generic;
+
+namespace ThreeOneSevenBee.ModelTests
+{
+    [TestClass]
+    public class RulesTests
+    {
+        [TestMethod]
+        public void Rules_ProductExponent()
+        {
+            ExpressionBase selection1;
+            ExpressionBase selection2;
+            ExpressionBase parent;
+
+            parent = ExpressionTests.Multiply(
+                selection1 = ExpressionTests.New("a"),
+                selection2 = ExpressionTests.New("a"));
+
+            var identity = Rules.ProductToExponentRule(parent, new List<ExpressionBase>() { selection1, selection2 });
+            Assert.IsNotNull(identity);
+            Assert.AreEqual(ExpressionTests.Power(ExpressionTests.New("a"), ExpressionTests.New(2)), identity.Suggestion);
+        }
+
+        [TestMethod]
+        public void Rules_ExponentProduct()
+        {
+            ExpressionBase selection1;
+            ExpressionBase selection2;
+            ExpressionBase parent;
+
+            parent = ExpressionTests.Power(
+                selection1 = ExpressionTests.New("a"),
+                selection2 = ExpressionTests.New(2));
+
+            var identity = Rules.ExponentToProductRule(parent, new List<ExpressionBase>() { selection1, selection2 });
+            Assert.IsNotNull(identity);
+            Assert.AreEqual(ExpressionTests.Multiply(ExpressionTests.New("a"), ExpressionTests.New("a")), identity.Suggestion);
+        }
+    }
+}
