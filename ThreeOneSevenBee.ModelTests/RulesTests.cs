a3fb81ddfeac250e0db61cdc9cea4392fd414d60
Nikolaj Mariager <nikolaj.mariager@gmail.com>
2016-04-14 09:37:06 +0200

    Added test for NumericVariadicRule and NumericBinaryRule.

--- ThreeOneSevenBee.ModelTests/RulesTests.cs
+++ ThreeOneSevenBee.ModelTests/RulesTests.cs
@@ -40,5 +40,68 @@ namespace ThreeOneSevenBee.ModelTests
             Assert.IsNotNull(identity);
             Assert.AreEqual(ExpressionTests.Multiply(ExpressionTests.New("a"), ExpressionTests.New("a")), identity.Suggestion);
         }
+
+        [TestMethod]
+        public void Rules_NumericVariadicRule()
+        {
+            ExpressionBase selection1;
+            ExpressionBase selection2;
+            ExpressionBase parent;
+            Identity identity;
+
+
+            parent = ExpressionTests.Add(
+                selection1 = ExpressionTests.New(2),
+                selection2 = ExpressionTests.New(2));
+
+            identity = Rules.NumericVariadicRule(parent, new List<ExpressionBase>() { selection1, selection2 });
+            Assert.IsNotNull(identity);
+            Assert.AreEqual(ExpressionTests.New(4), identity.Suggestion);
+
+
+            parent = ExpressionTests.Multiply(
+                selection1 = ExpressionTests.New(3),
+                selection2 = ExpressionTests.New(3));
+
+            identity = Rules.NumericVariadicRule(parent, new List<ExpressionBase>() { selection1, selection2 });
+            Assert.IsNotNull(identity);
+            Assert.AreEqual(ExpressionTests.New(9), identity.Suggestion);
+        }
+
+        [TestMethod]
+        public void Rules_NumericBinaryRule()
+        {
+            ExpressionBase selection1;
+            ExpressionBase selection2;
+            ExpressionBase parent;
+            Identity identity;
+
+
+            parent = ExpressionTests.Subtract(
+                selection1 = ExpressionTests.New(2),
+                selection2 = ExpressionTests.New(2));
+
+            identity = Rules.NumericBinaryRule(parent, new List<ExpressionBase>() { selection1, selection2 });
+            Assert.IsNotNull(identity);
+            Assert.AreEqual(ExpressionTests.New(0), identity.Suggestion);
+
+
+            /*parent = ExpressionTests.Divide(
+                selection1 = ExpressionTests.New(3),
+                selection2 = ExpressionTests.New(3));
+
+            identity = Rules.NumericBinaryRule(parent, new List<ExpressionBase>() { selection1, selection2 });
+            Assert.IsNotNull(identity);
+            Assert.AreEqual(ExpressionTests.New(1), identity.Suggestion);*/
+
+
+            parent = ExpressionTests.Power(
+                selection1 = ExpressionTests.New(3),
+                selection2 = ExpressionTests.New(3));
+
+            identity = Rules.NumericBinaryRule(parent, new List<ExpressionBase>() { selection1, selection2 });
+            Assert.IsNotNull(identity);
+            Assert.AreEqual(ExpressionTests.New(27), identity.Suggestion);
+        }
     }
 }
