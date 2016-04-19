e47774fa064fb5008187e0344fb47958c3b2d77e
Nikolaj Mariager <nikolaj.mariager@gmail.com>
2016-04-19 13:30:16 +0200

    Basic documentation for ExpressionBase.

--- ThreeOneSevenBee.Model/Expression/ExpressionBase.cs
+++ ThreeOneSevenBee.Model/Expression/ExpressionBase.cs
@@ -27,14 +27,35 @@ namespace ThreeOneSevenBee.Model.Expression
             return !(left == right);
         }
 
+        /// <summary>
+        /// The parent of this node. If this is null, this node is the root of the tree.
+        /// </summary>
         public ExpressionBase Parent { get; set; }
 
+        /// <summary>
+        /// A string representation of the value of the tree.
+        /// </summary>
         public abstract string Value { get; }
 
+        /// <summary>
+        /// Clones the tree recursively, making new references all the way.
+        /// </summary>
         public abstract ExpressionBase Clone();
 
+        /// <summary>
+        /// Does a reference replace, replacing the old expression with a clone of the replacement expression. Optionally does it recursively.
+        /// </summary>
+        /// <param name="old">The expression to replace.</param>
+        /// <param name="replacement">The expresison to replace with.</param>
+        /// <param name="doRecursively">Perform the replacement recursively.</param>
+        /// <returns>True if a replacement took place.</returns>
         public abstract bool Replace(ExpressionBase old, ExpressionBase replacement, bool doRecursively);
 
+        /// <summary>
+        /// Replaces this expression with the replacement expression if possible.
+        /// </summary>
+        /// <param name="replacement"></param>
+        /// <returns>True if a replacement took place.</returns>
         public bool Replace(ExpressionBase replacement)
         {
             if (Parent != null)
@@ -44,8 +65,15 @@ namespace ThreeOneSevenBee.Model.Expression
             return false;
         }
 
+        /// <summary>
+        /// Returns all elements in the tree recursively.
+        /// </summary>
         public abstract IEnumerable<ExpressionBase> GetNodesRecursive();
 
+        /// <summary>
+        /// Returns the path of nodes to get to the root of the tree.
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<ExpressionBase> GetParentPath()
         {
             ExpressionBase currentParent = this;
@@ -56,17 +84,38 @@ namespace ThreeOneSevenBee.Model.Expression
             }
         }
 
+        /// <summary>
+        /// Determines whether the expression can be calculated or not.
+        /// </summary>
+        /// <returns></returns>
         public abstract bool CanCalculate();
 
+        /// <summary>
+        /// Calculates the expression.
+        /// </summary>
+        /// <returns>The result or null if the tree cannot be calculated.</returns>
         public abstract double? Calculate();
 
+        /// <summary>
+        /// An arbitrary size score, to determine which expression is better.
+        /// </summary>
         public abstract int Size { get; }
 
+        /// <summary>
+        /// Determines if the other expression has the same value, but not reference.
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
         public override bool Equals(object other)
         {
             return (other is ExpressionBase) && Equals((ExpressionBase)other);
         }
 
+        /// <summary>
+        /// Determines if the other expression has the same value, but not reference.
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
         public abstract bool Equals(ExpressionBase other);
 
         public override int GetHashCode()
@@ -79,8 +128,14 @@ namespace ThreeOneSevenBee.Model.Expression
             return "{" + Value + "}";
         }
 
+        /// <summary>
+        /// Prints the tree to the standard output stream.
+        /// </summary>
         public abstract string TreePrint(string indent, bool isLast);
 
+        /// <summary>
+        /// Prints the tree to the standard output stream.
+        /// </summary>
         public void PrettyPrint()
         {
             this.TreePrint("", true);
