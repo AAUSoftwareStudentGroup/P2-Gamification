c8df83bb112addfbac262c014ebcbaa532874884
Lasse F. G <lasse-fribo@hotmail.com>
2016-05-12 13:07:20 +0200

    Changed size to 1 instead of 3

--- ThreeOneSevenBee.Model/Expression/Expressions/DelimiterExpression.cs
+++ ThreeOneSevenBee.Model/Expression/Expressions/DelimiterExpression.cs
@@ -59,7 +59,7 @@ namespace ThreeOneSevenBee.Model.Expression.Expressions
         {
             get
             {
-                return 3 + Expression.Size;
+                return 1 + Expression.Size;
             }
         }
 
