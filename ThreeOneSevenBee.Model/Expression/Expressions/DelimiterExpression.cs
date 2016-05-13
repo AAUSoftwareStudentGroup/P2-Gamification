c7743ffd8e0173df169c58a4b7166492afee0136
Lasse F. G <lasse-fribo@hotmail.com>
2016-05-13 09:27:14 +0200

    Setting size to 2 instead of one

--- ThreeOneSevenBee.Model/Expression/Expressions/DelimiterExpression.cs
+++ ThreeOneSevenBee.Model/Expression/Expressions/DelimiterExpression.cs
@@ -59,7 +59,7 @@ namespace ThreeOneSevenBee.Model.Expression.Expressions
         {
             get
             {
-                return 1 + Expression.Size;
+                return 2 + Expression.Size;
             }
         }
 
